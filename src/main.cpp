8303afe8f1df4a1d2516f607d0d35a53104fbbee
Aedrinios <clementclerc2@gmail.com>
2021-02-14 14:09:30 +0100

    dynamic load texture

--- src/main.cpp
+++ src/main.cpp
@@ -225,7 +225,7 @@ int main() {
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
 
-    unsigned int objTexture = loadTexture("resources/models/wildtextures_big-stone-wall-4k-480x480.jpg");
+    unsigned int objTexture = loadTexture(("resources/models/" + cube.getTextureName()).c_str());
     unsigned int cubemapTexture = loadCubemap(faces);
 
 	Shader skyboxShader("skybox");
